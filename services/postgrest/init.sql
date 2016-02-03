90773cfc6a2d0fce5e395b92aaa9808681043218
Michael Timbrook <timbrook480@gmail.com>
2016-02-02 22:59:15 -0500

    Updated postgres init script

--- services/postgrest/init.sql
+++ services/postgrest/init.sql
@@ -1,12 +1,20 @@
+-- Make things in the projects DB
 \c projects
 
 BEGIN;
+
+CREATE TABLE acconts
+(
+  id serial PRIMARY KEY,
+  prefered_name varchar(80),
+  email varchar(80)
+);
 
 CREATE TABLE requests
 (
   id serial PRIMARY KEY,
   title text NOT NULL,
-  year date NOT NULL
+  created date NOT NULL
 );
 
 COMMIT;

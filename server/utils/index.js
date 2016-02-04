3a3251c2db9bdac4c81687da8ba6192f7e5d0a29
Michael Timbrook <timbrook480@gmail.com>
2016-02-03 22:04:33 -0500

    Request insert put endpoint

--- server/utils/index.js
+++ server/utils/index.js
@@ -1,11 +1,32 @@
-const pg = require('pg-then');
+'use strict';
+
+const pg  = require('pg-then'),
+      sql = require('sql-template-strings')
+;
+
 const pool = pg.Pool('postgres://admin:alpine@192.168.99.100/projects');
 
 module.exports = {
-  createRequest (request) {
+  createRequest (r) {
 
+    let approverid = 1;
 
-    return new Promise( a => a() );
-
+    return pool.query(sql`
+      WITH approval AS (
+        INSERT INTO approval_status(approved_by) VALUES (${approverid}) RETURNING id
+      ) INSERT INTO public.requests(project, requester, part, unit_price, quantity, link, purpose, approval)
+        VALUES (${r.project}, 1, ${r.part}, ${r.unit_price}, ${r.quantity}, ${r.link}, ${r.purpose}, (select id from approval))
+        RETURNING id;
+    `)
+    .then(data => {
+      return data.rows;
+    })
+    .catch(err => {
+      if (err.message.indexOf("requests_project_fkey") > 0) {
+        let err  = new Error("Project does not exist")
+        err.status = 412;
+        throw err;
+      }
+    });
   }
 }
